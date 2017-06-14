f2f6c560f968850f17972ecca4a985791acc89d7
Alex Dickinson <alexdickinson04@gmail.com>
2017-06-13 22:34:19 -0400

    Initial committ from local
    ng 4 hero shit

---
+++ src/app/hero.js
@@ -0,0 +1,10 @@
+"use strict";
+var Hero = (function () {
+    function Hero() {
+    }
+    return Hero;
+}()); /**
+ * Created by Alex on 6/13/2017.
+ */
+exports.Hero = Hero;
+//# sourceMappingURL=hero.js.map
